b67ffbf86570f016e829a96588279542bce9c661
Tougo Kataita <105togo105togo@gmail.com>
2023-12-26 22:40:22 +0900

    test

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,16 +1,18 @@
-import rclpy
-from rclpy.node import Node
-from person_msgs.srv import Query
-
-def cb(request, response):
-    if request.name == "片板柊吾":
-        response.age = 44
-    else:
-        response.age = 255
-
-    return response
+import rclpy                     
+from rclpy.node import Node      
+from std_msgs.msg import Int16   
 
 rclpy.init()
-node = Node("talker")
-srv = node.create_service(Query, "query", cb)
+node = Node("talker")            
+pub = node.create_publisher(Int16, "countup", 10)   
+n = 0
+
+def cb():          
+    global n       
+    msg = Int16()  
+    msg.data = n   
+    pub.publish(msg)        
+    n += 1
+
+node.create_timer(0.5, cb)  
 rclpy.spin(node)
