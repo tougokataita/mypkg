9a04d71af8dcad7c370d8f4cf24eb8fc604a9e42
Tougo Kataita <105togo105togo@gmail.com>
2023-12-27 23:17:21 +0900

    talker listener

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,21 +1,16 @@
-import rclpy                     
-from rclpy.node import Node      
-from std_msgs.msg import Int16   
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
 
-class Talker():
-    def __init__(self, node):
-        self.pub = node.create_publisher(Int16, "countup", 10)
-        self.n = 0
-        node.create_timer(0.5, self.cb)
+def cb(request, response):
+    if request.name == "片板柊吾":
+        response.age = 44
+    else:
+        response.age = 255
 
-
-    def cb(self):               
-        msg = Int16()  
-        msg.data =self.n   
-        self.pub.publish(msg)        
-        self.n += 1
+    return response
 
 rclpy.init()
 node = Node("talker")
-talker = Talker(node)
+srv = node.create_service(Query, "query", cb)
 rclpy.spin(node)
