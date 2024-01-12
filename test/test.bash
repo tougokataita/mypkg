ea67ee5ed757623b89013fe950550634240085c5
Tougo Kataita <105togo105togo@gmail.com>
2024-01-13 01:14:45 +0900

    aaa

---
+++ test/test.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Tougo Kataita 　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+grep 'age: 44'
