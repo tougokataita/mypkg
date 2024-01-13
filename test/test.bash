41231beda6591415c55aa82b23d1057edb341afe
Tougo Kataita <105togo105togo@gmail.com>
2024-01-13 22:36:11 +0900

    aaa

--- test/test.bash
+++ test/test.bash
@@ -7,7 +7,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 2 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'age: 44'
